7bb7a0376bf583a4cdbf1b43f82386319f85baa5
Lucas Inocêncio <luki.inocencio@outlook.com>
2024-02-09 20:36:39 -0300

    Alterações na estilização, alterações no modal e formulário, alterações no envio de email, alteração no script do portifolio

--- public/js/index.js
+++ public/js/index.js
@@ -6,7 +6,7 @@ button.addEventListener('click', function() {
     mobileVavbar.classList.toggle('active');
 });
 
-// ativa e desativa o Shadow na rolagem da página
+// Ativa e desativa o Shadow na rolagem da página
 window.addEventListener('scroll', function() {
     if(this.window.scrollY > 0) return navbar.classList.add('active');
     return navbar.classList.remove('active');
@@ -29,7 +29,6 @@ function copiarEmail() {
     });
 }
 
-
 // Configuração do Modal
 
 document.getElementById('openModalBtn').addEventListener('click', function() {
@@ -43,27 +42,35 @@ document.getElementsByClassName('close')[0].addEventListener('click', function()
 document.getElementById('emailForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    // Aqui você pode adicionar o código para enviar o e-mail
-    // Neste exemplo, você pode apenas exibir os dados do formulário
-    let formData = new FormData(event.target);
-    formData.forEach((value, key) => {
-        console.log(key, ':', value);
-    });
-
-    // Fechar o modal após enviar
     document.getElementById('modal').style.display = 'none';
 });
 
 // Máscara para o campo telefone do modal
 const handlePhone = (event) => {
     let input = event.target
-    input.value = phoneMask(input.value)
+    input.value = mascaraTelefone(input.value)
   }
-  
-  const phoneMask = (value) => {
+
+  const mascaraTelefone = (value) => {
     if (!value) return ""
     value = value.replace(/\D/g,'')
     value = value.replace(/(\d{2})(\d)/,"($1) $2")
     value = value.replace(/(\d)(\d{4})$/,"$1-$2")
     return value
   }
+
+function enviaEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "luki.inocencio@outlook.com",
+        Password : "932485E3848B3EE2AFCE1DF3431F66387E28",
+        To : 'luki.inocencio@outlook.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : "Mensagem de: " + document.getElementById("name").value 
+            + "<br> Telefone para contato: " + document.getElementById("tel").value 
+            + "<br> Mensagem: "  + document.getElementById("message").value
+    }).then(
+    message => alert(message)
+    );
+}
