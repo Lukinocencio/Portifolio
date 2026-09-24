65bec569bcf17ddbf315ec426e683cd0c66b034a
Lucas Inocencio de Franca <luki.inocencio@outlook.com>
2026-09-24 12:11:24 -0300

    fix: corrige erro de exportacao do calendario do github

--- components/GithubCalendar.jsx
+++ components/GithubCalendar.jsx
@@ -1,7 +1,7 @@
 
 "use client";
 import React from "react";
-import GitHubCalendar from "react-github-calendar";
+import { GitHubCalendar } from "react-github-calendar";
 
 export default function GithubCalendarComponent() {
   return (
