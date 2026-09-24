c68f95a8ccf11bb4d69efee3d705d2e866b8c301
Lucas Inocencio de Franca <luki.inocencio@outlook.com>
2026-09-24 15:07:43 -0300

    Corrige cor da fonte no calendario do github de acordo com o tema

--- components/GithubCalendar.jsx
+++ components/GithubCalendar.jsx
@@ -2,6 +2,7 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
+import { useTheme } from "@/context/ThemeContext";
 
 const GitHubCalendar = dynamic(
   () => import("react-github-calendar").then((mod) => mod.GitHubCalendar),
@@ -9,10 +10,12 @@ const GitHubCalendar = dynamic(
 );
 
 export default function GithubCalendarComponent() {
+  const { theme } = useTheme();
+
   return (
     <div className="github-calendar-wrapper" style={{ marginTop: "40px", width: "100%", overflowX: "auto", paddingBottom: "15px" }}>
-      <div style={{ display: "table", margin: "0 auto", padding: "0 15px" }}>
-        <GitHubCalendar username="Lukinocencio" colorScheme="dark" />
+      <div style={{ display: "table", margin: "0 auto", padding: "0 15px", color: theme === "dark" ? "#fff" : "#000" }}>
+        <GitHubCalendar username="Lukinocencio" colorScheme={theme} />
       </div>
     </div>
   );
